d5b0b0059b9e960dec38b609d9195304e2b8d97b
umer987 <159330908+umer987@users.noreply.github.com>
2025-02-28 00:02:15 +0500

    Add files via upload

---
+++ simple quiz app/script.js
@@ -0,0 +1,37 @@
+
+
+let qu = document.querySelectorAll(".lab")
+
+
+qu.forEach( (event ,index)=> {
+console.log(event)
+
+})
+
+
+
+function checkAnswer(){
+    let qu = document.querySelectorAll(".lab")
+let re =document.querySelector(".result")
+    let total_que =15
+   
+    let score =0
+qu.forEach( (qusestion ,index)=> {
+    
+    console.log(qu)
+    let selectedOption = document.querySelector(`input[name="q${index+1}"]:checked`); // Get selected radio button
+
+if (selectedOption && selectedOption.value === "right") {
+    score+=1;
+
+    console.log(selectedOption)
+} 
+
+
+
+re.innerHTML=`✅you got ${score} out of ${total_que}  `
+})
+console.log(score)
+
+
+}
